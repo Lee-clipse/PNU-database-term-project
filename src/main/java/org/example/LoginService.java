a5d90e718df3b29635b09a9be6642e1e50e1659f
edac99 <jamjam1208@naver.com>
2023-12-26 20:55:53 +0900

    refactor: Class 관계 재구성
    MVC 패턴으로 구성

--- src/main/java/org/example/LoginService.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class LoginService {
-    public static String login(String userType, String userId) {
-        return userType.equalsIgnoreCase("manager") ? "ManagerClient" : "CustomerClient";
-    }
-}
